5b94e49103eb00c91aa20d1037361937d9bf9362
Antony Riley <antony@cyberiantiger.org>
2013-03-16 11:13:44 +0200

    Include wrapped exceptions for debugging purposes.

--- src/org/cyberiantiger/minecraft/unsafe/CBShim.java
+++ src/org/cyberiantiger/minecraft/unsafe/CBShim.java
@@ -9,6 +9,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import org.bukkit.Server;
+import org.bukkit.plugin.Plugin;
 
 /**
  *
@@ -19,26 +20,25 @@ public class CBShim {
     private static final String CRAFTBUKKIT_PACKAGE = "org.bukkit.craftbukkit";
     private static final Map<Class<?>,Object> cache = new HashMap<Class<?>, Object>();
 
-    public static <T> T getShim(Class<T> type, Server server, Object... args) {
+    public static <T> T createShim(Class<T> type, Plugin plugin, Object... args) {
         T ret = (T) cache.get(type);
         if (ret != null) {
             return ret;
         }
-        Class<?> serverClass = server.getClass();
+        Class<?> serverClass = plugin.getServer().getClass();
         while (!serverClass.getPackage().getName().startsWith(CRAFTBUKKIT_PACKAGE) ) {
             serverClass = serverClass.getSuperclass();
             if (serverClass == null) {
-                unsupportedVersion(server);
+                unsupportedVersion(plugin.getServer());
             }
         }
         String pkg  = serverClass.getPackage().getName();
         int i = pkg.lastIndexOf(".");
         if (i == -1) {
-            unsupportedVersion(server);
+            unsupportedVersion(plugin.getServer());
         }
         String childPackage = pkg.substring(i+1);
         String className = type.getClass().getPackage().getName() + '.' + childPackage + '.' + type.getSimpleName();
-        System.out.println("Loading class: " + className);
         try {
             Class<T> typeClass = (Class<T>) CBShim.class.getClassLoader().loadClass(className);
             Constructor[] constructors = typeClass.getConstructors();
@@ -58,22 +58,25 @@ public class CBShim {
             cache.put(type, ret);
             return ret;
         } catch (ClassNotFoundException ex) {
-            unsupportedVersion(server);
+            unsupportedVersion(plugin.getServer(), ex);
         } catch (InstantiationException ex) {
-            unsupportedVersion(server);
+            unsupportedVersion(plugin.getServer(), ex);
         } catch (IllegalAccessException ex) {
-            unsupportedVersion(server);
+            unsupportedVersion(plugin.getServer(), ex);
         } catch (IllegalArgumentException ex) {
-            unsupportedVersion(server);
+            unsupportedVersion(plugin.getServer(), ex);
         } catch (InvocationTargetException ex) {
-            unsupportedVersion(server);
+            unsupportedVersion(plugin.getServer(), ex);
         }
         // unreached, stupid compiler.
         return null;
     }
 
     private static void unsupportedVersion(Server server) {
-        throw new UnsupportedOperationException("Unsupported CraftBukkit version: " + server.getBukkitVersion());
+        unsupportedVersion(server, null);
+    }
+    private static void unsupportedVersion(Server server, Exception ex) {
+        throw new UnsupportedOperationException("Unsupported CraftBukkit version: " + server.getBukkitVersion(), ex);
     }
     
 }
